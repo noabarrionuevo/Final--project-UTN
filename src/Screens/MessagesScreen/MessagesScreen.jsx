85c0d1c4c485492d1fc2372e8de7fd9db7870206
Noa Barrionuevo | UX/UI Designer <noabarrionuevo3@gmail.com>
2025-12-17 16:59:14 -0300

    laoding screen

--- src/Screens/MessagesScreen/MessagesScreen.jsx
+++ src/Screens/MessagesScreen/MessagesScreen.jsx
@@ -4,6 +4,7 @@ import { ContactDetailContext } from '../../Context/ContactDetailContext'
 import HeaderSideBar from '../../Components/HeaderSideBar/HeaderSideBar'
 import { ContactsListContext } from '../../Context/ContactsListContext'
 import SectionChatsMessages from '../../Components/SectionChatsMessages/SectionChatsMessages'
+import LoadingScreen from '../../Components/LoadingScreen/LoadingScreen'
 import './MessagesScreen.css'
 
 
@@ -15,16 +16,15 @@ export default function MessagesScreen() {
     const { loadingContactsState } = useContext(ContactsListContext)
     return (
         <div className='MessagesScreens_App--container'>
+            {loadingContactsState && <LoadingScreen />}
             <HeaderSideBar />
             <ContactSidebar />
             {
-                loadingContactsState
-                    ? <h2>Cargando contactos...</h2>
-                    : contactSelected
-                        ? <div className='MessagesScreens_App--container--section_messages'>
-                            <SectionChatsMessages title={contactSelected.contact_name} avatar={contactSelected.contact_avatar} />
-                        </div>
-                        : <h2>Selecciona un contacto para ver los mensajes</h2>
+                contactSelected
+                    ? <div className='MessagesScreens_App--container--section_messages'>
+                        <SectionChatsMessages title={contactSelected.contact_name} avatar={contactSelected.contact_avatar} />
+                    </div>
+                    : <h2>Selecciona un contacto para ver los mensajes</h2>
             }
         </div>
     )
